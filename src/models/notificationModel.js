04bed934b5ac0bd9a4c72eb1a7e3a6fb81e61123
levanthang <levanthangthxd@gmail.com>
2019-09-11 23:11:49 +0700

    notification

--- src/models/notificationModel.js
+++ src/models/notificationModel.js
@@ -3,21 +3,61 @@ import mongoose from "mongoose";
 let Schema = mongoose.Schema;
 
 let NotificationSchema = new Schema({
-  sender: {
-    id: String,
-    username: String,
-    avatar: String,
-  },
-  receiver: {
-    id: String,
-    username: String,
-    avatar: String
-  },
+  senderId: String,
+  receiverId: String,
   type: String,
-  content: String,
   isRead: {type: Boolean, default: false},
   createAt: {type: Number, default: Date.now}
 });
 
+NotificationSchema.statics = {
+  createNew(item) {
+    return this.create(item);
+  },
+  removeRequestContactNotification(senderId, receiverId, type) {
+    return this.remove({
+      $and: [
+        {"senderId": senderId},
+        {"receiverId": receiverId},
+        {"type": type}
+      ]
+    }).exec();
+  },
+  /**
+   * get by userid va limit
+   * @param {String} userId 
+   * @param {String} limit 
+   */
+  getByUserIdAndLimit(userId, limit) {
+    return this.find({"receiverId": userId}).sort({"createdAt": -1}).limit(limit).exec();
+  }
+}
 
-module.exports = mongoose.model("notification", NotificationSchema);
+const NOTIICATION_TYPES = {
+  ADD_CONTACT: "add_contact"
+};
+
+const NOTIFICATION_CONTENS = {
+  getContent: (notificationType, isRead, userId, username, userAvatar) => {
+    if(notificationType === NOTIICATION_TYPES.ADD_CONTACT) {
+      if(!isRead) {
+        return `<span class="notif-readed-false" data-uid="${ userId }">
+        <img class="avatar-small" src="images/users/${ userAvatar }" alt=""> 
+        <strong>${ username }</strong> đã gửi cho bạn lời mời kết bạn !
+       </span><br><br><br>`
+      }
+      return `<span class="notif-readed-false" data-uid="${ userId }">
+      <img class="avatar-small" src="images/users/${ userAvatar }" alt=""> 
+      <strong>${ username }</strong> đã gửi cho bạn lời mời kết bạn !
+     </span><br><br><br>`
+  }
+      
+    return "No match with any notification type";
+  }
+};
+
+module.exports = {
+  model: mongoose.model("notification", NotificationSchema),
+  types: NOTIICATION_TYPES,
+  content: NOTIFICATION_CONTENS
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 import authService from "./authService";
 import userService from "./userService";
 import contactSerivice from "./contactService";
+import contactNotificationSerivice from "./notificationService";
 
 export const user = userService;
 export const auth = authService;
 export const contact = contactSerivice;
+export const notification = contactNotificationSerivice;
