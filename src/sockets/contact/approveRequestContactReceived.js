d5fabefd516035fb3c6db121436e82c27fd12378
levanthang <levanthangthxd@gmail.com>
2019-09-14 16:45:03 +0700

    approve_request_contact_received

---
+++ src/sockets/contact/approveRequestContactReceived.js
@@ -0,0 +1,38 @@
+import {pushSocketIdToArray, emitNotifyToArray, removeSocketIdFromArray} from "./../../helpers/socketHelper";
+/**
+ * 
+ * @param io from socket.io lib 
+ */
+let approveRequestContactReceived = (io) => {
+  let clients = {};
+  //moi lan dang nhap hay f5 hoac mo sang tab moi thi no tao ra mot socket id khac nhau vi vay ta luu cliens voi key : userid va value: mang socket id
+  //sau moi lan refresh ,f5 hay moi sang tab  moi ta xoa socket id cu va push sockeet id cu vao mang 
+  io.on("connection", (socket) => { //connnection khi nguoi dung vao trng web no sex chay
+    clients = pushSocketIdToArray(clients, socket.request.user._id, socket.id );
+   
+    socket.on("approve-request-contact-received", (data) => {
+      //console.log(data);
+      //console.log(socket.request.user); //muon lay cai nay thi phai cai them
+      //thu vien passports.socketio va cookieparse
+      let currentUser = {
+        id: socket.request.user._id,
+        username: socket.request.user.username,
+        avatar: socket.request.user.avatar,
+        address: (socket.request.user.address !== null) ? socket.request.user.address : ""
+      };
+      //emit notication
+      //khi no dang nhap
+      if(clients[data.contactId]) {
+        //vi du nguoi dung moi 2 tab thi goi cho ca 2 ben cua nguoi nhan
+        emitNotifyToArray(clients, data.contactId, io, "response-approve-request-contact-received", currentUser);
+      }
+    });
+    //khi f5  thi vao su kien nay
+    socket.on("disconnect", () => {
+      clients = removeSocketIdFromArray(clients, socket.request.user._id, socket);
+    });
+    console.log("cliens: ",clients);
+  });
+}
+
+module.exports = approveRequestContactReceived;
--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,6 +1,7 @@
 import addNewContact from "./contact/addNewContact";
 import removeRequestContactSent from "./contact/removeRequestContactSent";
 import removeRequestContactReceived from "./contact/removeRequestContactReceived";
+import approveRequestContactReceived from "./contact/approveRequestContactReceived";
 
 
 /**
@@ -11,6 +12,7 @@ let initSockets = (io) => {
   addNewContact(io);
   removeRequestContactSent(io);
   removeRequestContactReceived(io);
+  approveRequestContactReceived(io);
 }
 
 module.exports = initSockets;
